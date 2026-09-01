09443a4a8ef49704a1f3f4a119eba83f9f107c4d
FVS-MBA <feliven@gmail.com>
2026-09-01 17:49:38 -0300

    implementa método POST

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,11 @@ class Jogo:
         self.console = console
 
 
+jogo1 = Jogo(nome="Tetris", categoria="Puzzle", console="Atari 2600")
+jogo2 = Jogo(nome="Hollow Knight", categoria="Metroidvania", console="PS5")
+listaJogos = [jogo1, jogo2]
+
+
 @app.route("/inicio")
 def ola():
     return "<h1>olá mundo</h1>"
@@ -17,11 +22,23 @@ def ola():
 
 @app.route("/lista")
 def lista():
-    jogo1 = Jogo(nome="Tetris", categoria="Puzzle", console="Atari 2600")
-    jogo2 = Jogo(nome="Hollow Knight", categoria="Metroidvania", console="PS5")
-    listaJogos = [jogo1, jogo2]
 
     return render_template("lista.html", titulo="Lista", jogos=listaJogos)
+
+
+@app.route("/adicionar-jogo")
+def adicionar_jogo():
+    return render_template("adicionar-jogo.html", titulo="Novo jogo")
+
+
+@app.route("/criar", methods=["POST"])
+def criar():
+    nome = request.form["nome"]
+    categoria = request.form["categoria"]
+    console = request.form["console"]
+    jogo = Jogo(nome, categoria, console)
+    listaJogos.append(jogo)
+    return render_template("lista.html", titulo="Jogos", jogos=listaJogos)
 
 
 app.run(debug=True)
