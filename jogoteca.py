c60f5c4e72b2e09f016d6675d57674c549bd5ff7
FVS-MBA <feliven@gmail.com>
2026-09-01 18:11:54 -0300

    implementa bootstrap

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -15,14 +15,8 @@ jogo2 = Jogo(nome="Hollow Knight", categoria="Metroidvania", console="PS5")
 listaJogos = [jogo1, jogo2]
 
 
-@app.route("/inicio")
-def ola():
-    return "<h1>olá mundo</h1>"
-
-
-@app.route("/lista")
-def lista():
-
+@app.route("/")
+def index():
     return render_template("lista.html", titulo="Lista", jogos=listaJogos)
 
 
@@ -38,7 +32,7 @@ def criar():
     console = request.form["console"]
     jogo = Jogo(nome, categoria, console)
     listaJogos.append(jogo)
-    return render_template("lista.html", titulo="Jogos", jogos=listaJogos)
+    return redirect("/")
 
 
 app.run(debug=True)
