99dd971f8289d2d0b09486c46e039d3122674482
FVS-MBA <feliven@gmail.com>
2026-08-31 21:31:47 -0300

    implementa classe

--- jogoteca.py
+++ jogoteca.py
@@ -3,6 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+class Jogo:
+    def __init__(self, nome, categoria, console) -> None:
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 @app.route("/inicio")
 def ola():
     return "<h1>olá mundo</h1>"
@@ -10,7 +17,10 @@ def ola():
 
 @app.route("/lista")
 def lista():
-    listaJogos = ["God of War", "Skyrim", "Valorant"]
+    jogo1 = Jogo(nome="Tetris", categoria="Puzzle", console="Atari 2600")
+    jogo2 = Jogo(nome="Hollow Knight", categoria="Metroidvania", console="PS5")
+    listaJogos = [jogo1, jogo2]
+
     return render_template("lista.html", titulo="Lista", jogos=listaJogos)
 
 
