f81d2785058ea4e7e63228ec87ca60c675aeea2f
FVS-MBA <feliven@gmail.com>
2026-09-02 19:03:43 -0300

    adota rotas dinâmicas

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 app = Flask(__name__)
 app.secret_key = "CHAVE"
@@ -24,7 +24,7 @@ def index():
 @app.route("/adicionar-jogo")
 def adicionar_jogo():
     if ("usuario_logado" not in session) or (session["usuario_logado"] is None):
-        return redirect("/login?proxima=adicionar-jogo")
+        return redirect(url_for("login", proxima=url_for("adicionar_jogo")))
 
     return render_template("adicionar-jogo.html", titulo="Novo jogo")
 
@@ -36,7 +36,7 @@ def criar():
     console = request.form["console"]
     jogo = Jogo(nome, categoria, console)
     listaJogos.append(jogo)
-    return redirect("/")
+    return redirect(url_for("index"))
 
 
 @app.route("/login")
@@ -53,17 +53,19 @@ def autenticar():
     if "alohomora" == request.form["senha"]:
         session["usuario_logado"] = request.form["usuario"]
         flash(f'Usuário {session["usuario_logado"]} logado com sucesso')
-        return redirect(f"/{proxima_pagina}") if proxima_pagina else redirect("/login")
+        return (
+            redirect(proxima_pagina) if proxima_pagina else redirect(url_for("index"))
+        )
     else:
         flash("Usuário não logado")
-        return redirect("/login")
+        return redirect(url_for("login"))
 
 
 @app.route("/logout")
 def logout():
     session["usuario_logado"] = None
     flash("Logout efetuado com sucesso.")
-    return redirect("/")
+    return redirect(url_for("index"))
 
 
 app.run(debug=True, port=8000)
